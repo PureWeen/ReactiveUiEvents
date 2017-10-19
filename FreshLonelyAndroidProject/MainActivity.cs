f67c36cafebdc6414a04c07946cd91467a87497e
ShaneN <shane94@hotmail.com>
2017-10-19 07:38:30 -0600

    FreshLonelyAndroidProject

---
+++ FreshLonelyAndroidProject/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace FreshLonelyAndroidProject
+{
+    [Activity(Label = "FreshLonelyAndroidProject", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+
+            TextView view = new TextView(this);
+            view.Events().AfterTextChanged.Subscribe();
+        }
+    }
+}
+
