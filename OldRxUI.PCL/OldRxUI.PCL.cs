c28585b8b9f085f9559292c6ca5629ea6a5e4145
ShaneN <shane94@hotmail.com>
2017-10-19 00:03:47 -0600

    Add project files.

---
+++ OldRxUI.PCL/OldRxUI.PCL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace OldRxUI.PCL
+{
+    public class OldRxUIPCL : ContentPage
+	{
+		public OldRxUIPCL()
+		{
+			var button = new Button
+            {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Events().ChildrenReordered.Subscribe();
+
+			        Content = button;
+		}
+	}
+}
